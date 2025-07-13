1d5e83f394e1a7a2960866d104cdd6e050126069
Evan0A <wlsjordan01@gmail.com>
2025-07-13 16:29:54 +0700

    Update Event.lua

--- Event.lua
+++ Event.lua
@@ -1,11 +1,11 @@
-
+print("1")
 check_delay = 2
 loops = 0
 function checkCpuUsage(varlist, netid)
     loops = loops + 1
     print("HI, INI ADALAH LOOP KE: "..loops)
     print("im sedang turu")
-    sleep(5000)
+    sleep(20000)
 end
 
 addEvent(Event.varianlist, checkCpuUsage) 
